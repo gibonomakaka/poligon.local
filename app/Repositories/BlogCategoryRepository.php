9988981336abd2af057dc099b01111e15e577b9b
Maxim Gorbachov <maxim.gorbachov@ideus.biz>
2023-01-09 14:42:52 +0200

    Lesson 27 - Add repository

---
+++ app/Repositories/BlogCategoryRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogCategoryRepository
+ * @package App\Repositories
+ */
+class BlogCategoryRepository extends CoreRepository
+{
+    /**
+     * @return mixed|string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+    /**
+     * Получить модель для редактирования в админпанеле
+     * @param int $id
+     *
+     * @return Model
+     */
+    public function getEdit($id)
+    {
+        return $this->startConditions()->find($id);
+    }
+
+    /**
+     * Получить список категорий для вывода в выпадающем списке.
+     *
+     * @return Collection|\Illuminate\Database\Eloquent\Model[]|\Illuminate\Foundation\Application[]|mixed[]
+     */
+    public function getForComboBox()
+    {
+        return $this->startConditions()->all();
+    }
+}
